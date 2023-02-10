a033385161139072deed9e894190bccacf631ee9
linda-himesama <linda.himesama@gmail.com>
2023-02-09 20:34:34 -0500

    functionality added to the server

--- server.js
+++ server.js
@@ -7,4 +7,36 @@ const { getRandomElement } = require('./utils');
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+  });
 
+app.get('/api/quotes/random',(req,res,next) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send({quote: randomQuote});
+});
+
+app.get('/api/quotes',(req,res,next) => {
+    //console.log("the query is" + res.query);
+    if (req.query.person !== '') {
+        const personalQuotes = quotes.filter(quote => quote.person === req.query.person);
+        res.send({quotes: personalQuotes});
+    }else{
+    res.send({ quotes: quotes });
+    }
+});
+
+app.post('/api/quotes', (req,res,next) => {
+    if (req.query.quote !== '' && req.query.person !== ''){
+        let newQuote =
+        {
+            quote: req.query.quote,
+            person:  req.query.person
+        }
+
+        quotes.push(newQuote);
+        res.status(201).send({ quote: newQuote });
+    } else {
+        res.status(400).send();
+    }
+});
